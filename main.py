633cc8ab8eefc97e2cde5a61de73d0e003049c27
Caleb Berger <caleb.x.berger@gmail.com>
2018-09-17 12:48:47 -0400

    Temporarily remove "lexing" from main.py

--- main.py
+++ main.py
@@ -27,25 +27,4 @@ if __name__ == '__main__':
     e_types = ["transform", "cast", "shape", "damage"]
     type_ = random.choice(e_types)
     print("Effect type: " + type_)
-
-    if (type_ != "cast"):
-        s_shape = shaper.create_spell_shape()
-        print(s_shape)
-        final_range = ""
-        if (s_shape['shape']['type'] in ['cone', 'cylinder']):
-            final_range = "Range: {!s} foot tall {}".format(
-                s_shape['range'], s_shape['shape']['type'])
-        else:
-            final_range = "Range: {!s} foot {}".format(
-                s_shape['range'], s_shape['shape']['type'])
-        if ('radius' in s_shape['shape']):
-            # d = 2r, dumbass
-            final_range += ", {} feet across at the base".format(
-                s_shape['shape']['radius'] * 2)
-        if (s_shape['shape']['type'] != 'touch'):
-            center = rand_center()
-            final_range += ', centered on {}'.format(
-                center) if s_shape['shape']['type'] != 'cone' else ', with its apex centered on {}'.format(center)
-        print(final_range)
-    else:
-        print("Centered on " + rand_center())
+    print(shaper.create_spell_shape() if type_ != 'cast' else '')
