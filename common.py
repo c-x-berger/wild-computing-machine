e94c87f373cf2369d9ce318a5714a18dbadefc08
Caleb Berger <caleb.x.berger@gmail.com>
2018-09-19 12:02:18 -0400

    Add pluralizer to common

--- common.py
+++ common.py
@@ -1,7 +1,12 @@
 import random
 
+
 def roll_ndn(dice: list) -> list:
     rolls = []
     for _ in range(dice[0]):
         rolls.append(random.randint(1, dice[1]))
     return rolls
+
+
+def plural(in_: str) -> str:
+    return in_ + 's' if in_ != 'wolf' else 'wolves'
