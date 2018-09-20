2af1b37d1e0c329014f5b277734c57c5e2f61186
Caleb Berger <caleb.x.berger@gmail.com>
2018-09-20 16:02:15 -0400

    Package for PyPi

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='wild_computing_machine',
+    version="0.0.1",
+    author="Caleb Xavier Berger",
+    author_email="caleb.x.berger@gmail.com",
+    description="Chaotic Stupid D&D spell generator",
+    url="https://github.com/c-x-berger/wild-computing-machine",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
